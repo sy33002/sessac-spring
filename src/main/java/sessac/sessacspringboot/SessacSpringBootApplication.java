7184f6310fbc92e14ac55d5d6725e9fad5a4533d
sy33002 <sy33002@naver.com>
2023-11-29 16:41:29 +0900

    11/29

---
+++ src/main/java/sessac/sessacspringboot/SessacSpringBootApplication.java
@@ -0,0 +1,13 @@
+package sessac.sessacspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessacSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessacSpringBootApplication.class, args);
+	}
+
+}
