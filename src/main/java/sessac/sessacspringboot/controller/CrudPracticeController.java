1241d9d4f340ddfec600f6fa078de8c44138fabb
sy33002 <sy33002@naver.com>
2023-12-01 18:59:58 +0900

    12/1: 실습진행중

--- src/main/java/sessac/sessacspringboot/controller/CrudPracticeController.java
+++ src/main/java/sessac/sessacspringboot/controller/CrudPracticeController.java
@@ -1,10 +1,21 @@
 package sessac.sessacspringboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import sessac.sessacspringboot.dto.UserPrac;
+
 @Controller
 public class CrudPracticeController {
     @GetMapping("/register/practice")
     public String getRegister() {return "register";}
 
+    @PostMapping("/register/practice")
+    public String postRegister(@RequestBody UserPrac userPrac, Model model) {
+        model.addAttribute("data", userPrac);
+        return "userDetail";
+    }
 }
---
+++ src/main/java/sessac/sessacspringboot/dto/UserPrac.java
@@ -0,0 +1,12 @@
+package sessac.sessacspringboot.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserPrac {
+    private String userId;
+    private String password;
+    private String email;
+}
